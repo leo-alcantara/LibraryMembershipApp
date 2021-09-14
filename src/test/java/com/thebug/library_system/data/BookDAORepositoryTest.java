7878ddc13b23ea859e1453766de74a96934044f8
leoodindealcantara <leoalcantara@icloud.com>
2021-09-14 23:58:37 +0200

    Entities and repositories implemented + tests created

---
+++ src/test/java/com/thebug/library_system/data/BookDAORepositoryTest.java
@@ -0,0 +1,33 @@
+package com.thebug.library_system.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDAORepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
